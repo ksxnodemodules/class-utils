3a2c30b36f65e28dface075ebf6d29f81fe76401
KSXGitHub <hvksmr1996@gmail.com>
2015-12-26 23:31:43 +0700

    add feature

--- index.js
+++ index.js
@@ -6,36 +6,49 @@
 		createClass: createClass
 	};
 
-	function createClass(prototype, constructor) {
+	function createClass(prototype, constructor, ...args) {
 		if (typeof constructor !== 'function') {
 			constructor = () => {};
 		}
 		result.prototype = prototype;
-		return createClass.super(result);
+		return createClass.super(result, ...args);
 		function result(...args) {
 			return constructor.call(this, ...args);
 		}
 	}
 
-	createClass.super = (Super) =>
-		class extends (typeof Super === 'function' ? Super : createClass.super.default) {
+	createClass.super = (Super, options) => {
+
+		class Result extends (typeof Super === 'function' ? Super : createClass.super.default) {
+
 			constructor(...args) {
 				super(...args);
 			}
+
+			static get create() {
+				return (...args) => new this(...args);
+			}
+
 		};
 
-	createClass.super.default = class {
-		static get create() {
-			return (...args) => new this(...args);
+		options = Object(options);
+
+		if (options.setproto) {
+			Object.setPrototypeOf(Result, Super);
 		}
-	};
+
+		return Result;
+
+	}
+
+	createClass.super.default = class {};
 
 	createClass.super.handleArgs = (Super, handle) =>
-		class extends createClass.super(Super) {
+		createClass.super(class extends createClass.super(Super) {
 			constructor(...args) {
 				super(...handle(...args));
 			}
-		};
+		});
 
 	createClass.super.handleArgs.packer = (Super) =>
 		createClass.super.handleArgs(Super, (...args) => [args]);
